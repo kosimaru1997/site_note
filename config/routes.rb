1c9941b7db9a3ee79964690d0d761e6b086ef5b3
kosimaru1997 <kosimaru1997@gmail.com>
2021-08-09 11:30:55 +0900

    destroy user_name_column

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
-  root to: 'homes#top'
+
+  unauthenticated :user do
+    root to: 'homes#top', as: :unauthenticated_root
+  end
+
+  authenticated :user do
+    root to: 'users#show'
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
