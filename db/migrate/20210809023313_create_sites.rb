c5aada83da042defa001eca3d662aa2b3d3dc33c
kosimaru1997 <kosimaru1997@gmail.com>
2021-08-12 09:35:02 +0900

    fix site_table

--- db/migrate/20210809023313_create_sites.rb
+++ db/migrate/20210809023313_create_sites.rb
@@ -3,13 +3,15 @@ class CreateSites < ActiveRecord::Migration[6.1]
     create_table :sites do |t|
       t.references :user, foreign_key: true, null: false
       t.string :url, null: false
-      t.string :title
+      t.string :title, default: "No Title"
+      t.text :description, default: "No Description"
       t.text :image
       t.text :note
 
       t.timestamps
     end
     add_index :sites, [:title]
+    add_index :sites, [:description]
     add_index :sites, [:note]
   end
 end
