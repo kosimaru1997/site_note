eb9e36731e88f3c1566d56d2f81ce8935dbef65d
kosimaru1997 <kosimaru1997@gmail.com>
2021-08-13 20:47:00 +0900

    set initializer for deploy

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 initializer "devise.configure_zeitwerk_if_enabled" do
   if Rails.autoloaders.zeitwerk_enabled? && !defined?(ActionMailer)
     Rails.autoloaders.main.ignore("#{__dir__}/relative/path/to/devise/mailer.rb")
