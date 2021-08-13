f9ee35ed29281c2ea5c1db67b0b9d2acb9b5a4e1
kosimaru1997 <kosimaru1997@gmail.com>
2021-08-13 20:48:22 +0900

    set initializer for deploy

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
-initializer "devise.configure_zeitwerk_if_enabled" do
-  if Rails.autoloaders.zeitwerk_enabled? && !defined?(ActionMailer)
-    Rails.autoloaders.main.ignore("#{__dir__}/relative/path/to/devise/mailer.rb")
-  end
-end
-
 # Assuming you have not yet modified this file, each configuration option below
 # is set to its default value. Note that some are commented out while others
 # are not: uncommented lines are intended to protect your configuration from
@@ -15,6 +9,13 @@ end
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+
+  initializer "devise.configure_zeitwerk_if_enabled" do
+    if Rails.autoloaders.zeitwerk_enabled? && !defined?(ActionMailer)
+      Rails.autoloaders.main.ignore("#{__dir__}/relative/path/to/devise/mailer.rb")
+    end
+  end
+  
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
@@ -30,7 +31,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  # config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
