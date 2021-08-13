3834349bfba5b4cba3479aaded608f9c09a318d3
kosimaru1997 <kosimaru1997@gmail.com>
2021-08-13 20:50:41 +0900

    set initializer for deploy

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,13 +9,6 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-
-  initializer "devise.configure_zeitwerk_if_enabled" do
-    if Rails.autoloaders.zeitwerk_enabled? && !defined?(ActionMailer)
-      Rails.autoloaders.main.ignore("#{__dir__}/relative/path/to/devise/mailer.rb")
-    end
-  end
-  
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
