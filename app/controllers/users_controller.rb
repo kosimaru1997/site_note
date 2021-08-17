5f5623a87b6b84c5a8eb9571efd9dabdd361698c
kosimaru1997 <kosimaru1997@gmail.com>
2021-08-17 12:40:18 +0900

    finish pagy-bootstrap-deco

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @site = Site.new
-    @pagy, @sites = pagy(current_user.sites.reverse_order, items: 9)
+    @pagy, @sites = pagy(current_user.sites.reverse_order, items: 21)
   end
 
   def edit
